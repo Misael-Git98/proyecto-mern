d802648e1b81f1d97cd3736496cabc48f5fc0df0
RodriSebastian <rodrigoriquelme2198@gmail.com>
2024-11-30 23:14:57 -0300

    add task-crud

--- src/routes/tasksRoutes.js
+++ src/routes/tasksRoutes.js
@@ -1,8 +1,23 @@
 import {Router} from 'express';
 import { authRequired } from '../middlewares/validateToken';
+import { 
+    getTasks, 
+    getTask, 
+    createTasks, 
+    updateTasks, 
+    deleteTasks, 
+} from '../controllers/taskController.js';
 
 const router = Router()
 
-router.get('/tasks', authRequired, (req, res) => res.send('tasks'))
+router.get('/tasks', authRequired, getTasks);
+
+router.get('/tasks/:id', authRequired, getTask);
+
+router.post('/tasks', authRequired, createTasks);
+
+router.delete('/tasks/:id', authRequired, deleteTasks);
+
+router.put('/tasks/:id', authRequired, updateTasks);
 
 export default router
